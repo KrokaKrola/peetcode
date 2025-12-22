63ada804fe28f0dfe215bf0b1583f12ef4032c76
KrokaKrola <harlamsan@gmail.com>
2025-12-22 11:33:17 +0200

    15 one more time

--- two-pointers/15.py
+++ two-pointers/15.py
@@ -27,4 +27,37 @@ def threeSum(nums: list[int]) -> list[list[int]]:
     return result
 
 
+def threeSum2(nums: list[int]) -> list[list[int]]:
+    nums = sorted(nums)
+
+    results = []
+
+    for i in range(len(nums)):
+        if i > 1 and nums[i] == nums[i - 1]:
+            continue
+
+        lp = i + 1
+        rp = len(nums) - 1
+
+        while lp < rp:
+            sum = nums[i] + nums[lp] + nums[rp]
+
+            if sum < 0:
+                lp += 1
+            elif sum > 0:
+                rp -= 1
+            else:
+                results.append([nums[i], nums[lp], nums[rp]])
+                lp += 1
+
+                while lp < rp:
+                    if nums[lp] == nums[lp + 1]:
+                        lp += 1
+                    else:
+                        break
+
+    return results
+
+
 print(threeSum([-1, 0, 1, 2, -1, -4]))
+print(threeSum2([-1, 0, 1, 2, -1, -4]))
