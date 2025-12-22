690f9a915cb25811dd5ff6cda4c286c6cfb090f4
KrokaKrola <harlamsan@gmail.com>
2025-12-22 11:21:24 +0200

    977 one more time

--- two-pointers/977.py
+++ two-pointers/977.py
@@ -16,4 +16,23 @@ def sortedSquares(nums: list[int]) -> list[int]:
     return output
 
 
+def sortedSquares2(nums: list[int]) -> list[int]:
+    n = len(nums)
+    results = [0] * n
+
+    left = 0
+    right = n - 1
+
+    for i in range(n - 1, -1, -1):
+        if abs(nums[left]) < abs(nums[right]):
+            results[i] = nums[right] * nums[right]
+            right -= 1
+        else:
+            results[i] = nums[left] * nums[left]
+            left += 1
+
+    return results
+
+
 print(sortedSquares([-4, -1, 0, 1, 2, 3, 5]))
+print(sortedSquares2([-4, -1, 0, 1, 2, 3, 5]))
