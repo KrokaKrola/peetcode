2536afc55a986134fe53c807afe44db346e88926
KrokaKrola <harlamsan@gmail.com>
2025-12-24 11:05:58 +0200

    sliding-window refresher

--- sliding-window/209.py
+++ sliding-window/209.py
@@ -17,8 +17,24 @@ def minSubArrayLen(target: int, nums: list[int]) -> int:
     return min_length
 
 
-# print(minSubArrayLen(7, [2, 3, 1, 2, 4, 3]))
-# print(minSubArrayLen(4, [1, 4, 4]))
-# print(minSubArrayLen(5, [5]))
-# print(minSubArrayLen(11, [1, 1, 1, 1, 1, 1, 1]))
-print(minSubArrayLen(11, [1, 2, 3, 4, 5]))
+def minSubArrayLen2(target: int, nums: list[int]) -> int:
+    left = 0
+    min_len = float("inf")
+    curr_sum = 0
+
+    for right in range(len(nums)):
+        curr_sum += nums[right]
+
+        while curr_sum >= target:
+            min_len = min(min_len, right - left + 1)
+            curr_sum -= nums[left]
+            left += 1
+
+    return min_len if min_len != float("inf") else 0
+
+
+print(minSubArrayLen2(7, [2, 3, 1, 2, 4, 3]))
+print(minSubArrayLen2(4, [1, 4, 4]))
+print(minSubArrayLen2(5, [5]))
+print(minSubArrayLen2(11, [1, 1, 1, 1, 1, 1, 1]))
+print(minSubArrayLen2(11, [1, 2, 3, 4, 5]))
--- sliding-window/643.py
+++ sliding-window/643.py
@@ -11,8 +11,21 @@ def findMaxAverage(nums: list[int], k: int) -> float:
     return max_sum / k
 
 
-print(findMaxAverage([1, 12, -5, -6, 50, 3], 4), "12.75")
-print(findMaxAverage([0, 1, 1, 3, 3], 4), "1.25")
-print(findMaxAverage([5], 1), "5.0")
-print(findMaxAverage([-1], 1), "-1.0")
-print(findMaxAverage([0, 1, 1, 3, 3], 4), "2.0")
+def findMaxAverage2(nums: list[int], k: int) -> float:
+    max_sum = sum(nums[:k])
+    curr_sum = max_sum
+
+    for i in range(1, len(nums) - k + 1):
+        new_elem = nums[i + k - 1]
+        old_elem = nums[i - 1]
+        curr_sum = curr_sum + new_elem - old_elem
+        max_sum = max(curr_sum, max_sum)
+
+    return max_sum / k
+
+
+print(findMaxAverage2([1, 12, -5, -6, 50, 3], 4), "12.75")
+print(findMaxAverage2([0, 1, 1, 3, 3], 4), "1.25")
+print(findMaxAverage2([5], 1), "5.0")
+print(findMaxAverage2([-1], 1), "-1.0")
+print(findMaxAverage2([0, 1, 1, 3, 3], 4), "2.0")
