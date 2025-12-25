1df5a8a824c37dc7fb325a0c22eb29d71e7683f7
KrokaKrola <harlamsan@gmail.com>
2025-12-25 21:47:24 +0200

    sliding-window/567.py without direct dict comparison

--- sliding-window/567.py
+++ sliding-window/567.py
@@ -34,6 +34,55 @@ def checkInclusion(str1: str, str2: str) -> bool:
     return False
 
 
-print(checkInclusion("ab", "eidbaooo"), True)
-print(checkInclusion("ab", "eidboaoo"), False)
-print(checkInclusion("adc", "dcda"), True)
+def checkInclusion2(str1: str, str2: str) -> bool:
+    if len(str1) > len(str2):
+        return False
+
+    str1_dict = dict()
+
+    for el in str1:
+        if el in str1_dict:
+            str1_dict[el] += 1
+        else:
+            str1_dict[el] = 1
+
+    str2_dict = {char: 0 for char in str1_dict}
+    matches = 0
+    window_size = len(str1)
+    left = 0
+
+    for right in range(len(str2)):
+        right_char = str2[right]
+
+        if right_char in str1_dict:
+            if str2_dict[right_char] == str1_dict[right_char]:
+                matches -= 1
+
+            str2_dict[right_char] += 1
+
+            if str2_dict[right_char] == str1_dict[right_char]:
+                matches += 1
+
+        if right - left + 1 > window_size:
+            left_char = str2[left]
+
+            if left_char in str1_dict:
+                if str2_dict[left_char] == str1_dict[left_char]:
+                    matches -= 1
+
+                str2_dict[left_char] -= 1
+
+                if str2_dict[left_char] == str1_dict[left_char]:
+                    matches += 1
+
+            left += 1
+
+        if matches == len(str1_dict):
+            return True
+
+    return False
+
+
+print(checkInclusion2("ab", "eidbaooo"), True)
+print(checkInclusion2("ab", "eidboaoo"), False)
+print(checkInclusion2("adc", "dcda"), True)
