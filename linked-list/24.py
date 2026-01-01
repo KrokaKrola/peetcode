5d2f795977a4bce8586eef477908127d6b011b2b
KrokaKrola <harlamsan@gmail.com>
2026-01-01 22:59:25 +0200

    linked-list/24.py

---
+++ linked-list/24.py
@@ -0,0 +1,53 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self):
+        values = []
+        current = self
+        while current:
+            values.append(str(current.val))
+            current = current.next
+        return " -> ".join(values)
+
+
+def build_list(values: list) -> Optional[ListNode]:
+    if not values:
+        return None
+
+    head = ListNode(values[0])
+    current = head
+
+    for val in values[1:]:
+        current.next = ListNode(val)
+        current = current.next
+
+    return head
+
+
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        prev = dummy
+
+        while prev.next and prev.next.next:
+            first = prev.next
+            second = prev.next.next
+
+            first.next = second.next
+            second.next = first
+            prev.next = second
+
+            prev = first
+
+        return dummy.next
+
+
+print(Solution().swapPairs(build_list([1, 2, 3, 4])), "=> 2 -> 1 -> 4 -> 3")
+print(Solution().swapPairs(build_list([])), "")
+print(Solution().swapPairs(build_list([1])), "1")
+print(Solution().swapPairs(build_list([1, 2, 3])), "=> 2 -> 1 -> 3")
