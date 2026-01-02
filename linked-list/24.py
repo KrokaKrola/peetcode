da72723475d1a19cb2f0ee65093e96c9d6f11d77
KrokaKrola <harlamsan@gmail.com>
2026-01-02 09:35:24 +0200

    linked-list/24.py

--- linked-list/24.py
+++ linked-list/24.py
@@ -46,8 +46,24 @@ class Solution:
 
         return dummy.next
 
+    def swapPairs2(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        dummy.next = head
+        current = dummy
 
-print(Solution().swapPairs(build_list([1, 2, 3, 4])), "=> 2 -> 1 -> 4 -> 3")
-print(Solution().swapPairs(build_list([])), "")
-print(Solution().swapPairs(build_list([1])), "1")
-print(Solution().swapPairs(build_list([1, 2, 3])), "=> 2 -> 1 -> 3")
+        while current.next and current.next.next:
+            first = current.next
+            second = current.next.next
+
+            first.next = second.next
+            second.next = first
+            current.next = second
+            current = first
+
+        return dummy.next
+
+
+print(Solution().swapPairs2(build_list([1, 2, 3, 4])), "=> 2 -> 1 -> 4 -> 3")
+print(Solution().swapPairs2(build_list([])), "")
+print(Solution().swapPairs2(build_list([1])), "1")
+print(Solution().swapPairs2(build_list([1, 2, 3])), "=> 2 -> 1 -> 3")
