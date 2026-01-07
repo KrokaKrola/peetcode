1eee5b4da3adcbba86bbc19a5c1b452dce67b12b
KrokaKrola <harlamsan@gmail.com>
2026-01-07 17:28:37 +0200

    225.py

--- stack-queue/225.py
+++ stack-queue/225.py
@@ -18,15 +18,33 @@ class MyStack:
         return target
 
     def top(self) -> int:
-        target = self.pop()
-        self.queue.append(target)
-        return target
+        return self.queue[0]
 
     def empty(self) -> bool:
         return len(self.queue) == 0
 
 
-myStack = MyStack()
+class MyStack2:
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+        for _ in range(len(self.queue) - 1):
+            self.queue.append(self.queue.popleft())
+
+    def pop(self) -> int:
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        return len(self.queue) == 0
+
+
+myStack = MyStack2()
 myStack.push(1)
 myStack.push(2)
 print(myStack.top())
