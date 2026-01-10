1dd58450be88efb09a697e8c3c1637656f710dc9
KrokaKrola <harlamsan@gmail.com>
2026-01-10 23:00:29 +0200

    901.py

--- stack-queue/901.py
+++ stack-queue/901.py
@@ -1,18 +1,13 @@
 class StockSpanner:
     def __init__(self):
-        self.spans = []
-        self.prices = []
+        self.stack = []
 
     def next(self, price: int) -> int:
         span = 1
-
-        while self.prices and self.prices[-1] <= price:
-            span += self.spans[-1]
-            self.spans.pop()
-            self.prices.pop()
-
-        self.spans.append(span)
-        self.prices.append(price)
+        while self.stack and self.stack[-1][0] <= price:
+            span += self.stack[-1][1]
+            self.stack.pop()
+        self.stack.append((price, span))
 
         return span
 
