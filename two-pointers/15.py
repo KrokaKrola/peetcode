aa26229fa2304ca24dee76f415be4519f719ee10
KrokaKrola <harlamsan@gmail.com>
2026-05-17 13:19:14 +0300

    15.py

--- two-pointers/15.py
+++ two-pointers/15.py
@@ -1,63 +1,35 @@
-def threeSum(nums: list[int]) -> list[list[int]]:
-    nums = sorted(nums)
-    result = []
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        results = []
 
-    n = len(nums)
+        for i in range(len(nums)):
+            # skipping same element as before, for not first element
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
 
-    for i in range(n):
-        if i > 0 and nums[i] == nums[i - 1]:
-            continue
+            left = i + 1
+            right = len(nums) - 1
+            target = -1 * nums[i]
 
-        lp = i + 1
-        rp = n - 1
+            while left < right:
+                tmp_sum = nums[left] + nums[right]
+                if tmp_sum > target:
+                    right -= 1
+                elif tmp_sum < target:
+                    left += 1
+                else:
+                    results.append([nums[i], nums[left], nums[right]])
+                    left += 1
 
-        while lp < rp:
-            total = nums[i] + nums[lp] + nums[rp]
-            if total > 0:
-                rp -= 1
-            elif total < 0:
-                lp += 1
-            else:
-                result.append([nums[i], nums[lp], nums[rp]])
-                lp += 1
+                    # move forward for for consecutive elements with the same value as lts left value
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
 
-                while nums[lp] == nums[lp - 1] and lp < rp:
-                    lp += 1
-
-    return result
-
-
-def threeSum2(nums: list[int]) -> list[list[int]]:
-    nums = sorted(nums)
-
-    results = []
-
-    for i in range(len(nums)):
-        if i > 1 and nums[i] == nums[i - 1]:
-            continue
-
-        lp = i + 1
-        rp = len(nums) - 1
-
-        while lp < rp:
-            sum = nums[i] + nums[lp] + nums[rp]
-
-            if sum < 0:
-                lp += 1
-            elif sum > 0:
-                rp -= 1
-            else:
-                results.append([nums[i], nums[lp], nums[rp]])
-                lp += 1
-
-                while lp < rp:
-                    if nums[lp] == nums[lp + 1]:
-                        lp += 1
-                    else:
-                        break
-
-    return results
+        return results
 
 
-print(threeSum([-1, 0, 1, 2, -1, -4]))
-print(threeSum2([-1, 0, 1, 2, -1, -4]))
+# print(Solution().threeSum([-1, 0, 1, 2, -1, -4]), "=", [[-1, -1, 2], [-1, 0, 1]])
+# print(Solution().threeSum([0, 1, 1]), [])
+# print(Solution().threeSum([0, 0, 0]), [0, 0, 0])
+print(Solution().threeSum([0, 0, 0, 0]), [0, 0, 0])
